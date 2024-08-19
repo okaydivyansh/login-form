60836c8cfaffaf92046b7de3f24c1d921288c2e4
Divyansh Mishra <divyanshm09@gmail.com>
2024-08-19 23:07:47 +0530

    lol

--- src/components/signup/Form.jsx
+++ src/components/signup/Form.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
-
+import email_icon from '../../Assets/email.png'
+import password_icon from '../../Assets/password.png'
+import person_icon from '../../Assets/person.png'
 const Form = () => {
   return (
-    <div>
-      
+    <div className="mt-50 m-8 flex flex-col pb-30">
+      <div className='header flex-1 w-32 ...'>
+        <div className='text'> Sign Up </div>
+        <div className='underline'></div>
+      </div>
+      <div className='inputs'>
+        <div className='input'>
+          <img src={person_icon} alt=''/>
+          <input type="text"/>
+        </div>
+        <div className='input'>
+          <img src={email_icon} alt=''/>
+          <input type='email'/>
+        </div>
+        <div className='input'>
+          <img src={password_icon} alt=''/>
+          <input type='password'/>
+        </div>
+      </div>
+      <div className='forgot-password'>Lost Password? <span class="flex justify-end ">Click Here!</span></div>
+      <div className='submit-container'>
+        <div className='submit'>Sign up</div>
+        <div className='submit'>Login</div>
+      </div>
     </div>
   )
 }
